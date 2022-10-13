c0bc4d3f35c0a7f5fe87b06f32fc1bfcf2f18fd5
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2022-10-13 19:04:32 -0300

    Configuração do storybook e design system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,34 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    colors: {
+
+      transparent: 'transparent',
+
+      white: '#fff',
+      black: '#000',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1fb',
+      },
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
