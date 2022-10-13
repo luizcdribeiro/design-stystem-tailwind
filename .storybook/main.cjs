f4fb462cc29d128d214ad9e0d07cb75a3b6e184b
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2022-10-13 19:36:55 -0300

    ci: Adicionar publicação storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/design-stystem-tailwind/'
+    }
+    return config
   }
 }
